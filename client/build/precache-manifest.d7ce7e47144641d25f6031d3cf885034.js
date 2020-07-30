020cc4116016be6e807bba84155420bb26559bd9
Majd Sabik <majdsabik.ms@gmail.com>
2020-07-30 22:53:38 +0200

    The final build

---
+++ client/build/precache-manifest.d7ce7e47144641d25f6031d3cf885034.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650c90a95b814de377f22386d76f903f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c65ecbaddad260ca49",
+    "url": "/static/css/main.ec10ce61.chunk.css"
+  },
+  {
+    "revision": "5a0073f9c867148477ee",
+    "url": "/static/js/2.02420f61.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.02420f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c65ecbaddad260ca49",
+    "url": "/static/js/main.0dbf73ad.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4a29cf31dfcc4085440e04516ef7ed93",
+    "url": "/static/media/cloud.4a29cf31.svg"
+  }
+]);
